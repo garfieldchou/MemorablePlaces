5cd641250663d991d12eea5aec0e2fcc4b47ea10
shaudai <shaudai0402@hotmail.com>
2017-01-15 21:37:44 +0800

    Add a ListView with one item in MainActivity

--- app/src/main/java/com/garfieldchou/memorableplaces/MainActivity.java
+++ app/src/main/java/com/garfieldchou/memorableplaces/MainActivity.java
@@ -2,12 +2,41 @@ package com.garfieldchou.memorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView placeListView;
+    ArrayList<String> placeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        placeListView = (ListView) findViewById(R.id.placeListView);
+
+        placeList = new ArrayList<String>();
+
+        placeList.add("Add a new place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, placeList);
+
+        placeListView.setAdapter(arrayAdapter);
+
+        placeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.i("item pressed", Integer.toString(i));
+
+            }
+        });
     }
 }
