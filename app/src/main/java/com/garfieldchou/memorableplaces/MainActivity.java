ec997c12d0b569e2ad564d55e56f05cf8709a7c0
shaudai <shaudai0402@hotmail.com>
2017-01-18 23:24:02 +0800

    Set ListView and corresponding ArrayList & OnItemClickListener() at MainActivity

--- app/src/main/java/com/garfieldchou/memorableplaces/MainActivity.java
+++ app/src/main/java/com/garfieldchou/memorableplaces/MainActivity.java
@@ -2,6 +2,13 @@ package com.garfieldchou.memorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        final ArrayList<String> places = new ArrayList<>();
+
+        places.add("Add a new places...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, String.valueOf(i), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
